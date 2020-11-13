18402320413ac50973776cd381576f6bb58f31a5
ja.toctaguano <ja.toctaguano@alumnos.urjc.es>
2020-11-14 00:36:39 +0100

    mods

--- Sources/echo-client/main.swift
+++ Sources/echo-client/main.swift
@@ -1,1 +1,24 @@
-print("Hello, world!")
+/* Client echo UDP.... */
+
+import Socket
+import Glibc
+
+let server = "localhost"
+let port: Int32 = 7667
+let message = "Hello, world!"
+
+do {
+    guard let serverAddress = Socket.createAddress(for: server, on: port) else {
+        print("Error creating Address")
+        exit(1)
+        
+    }
+
+    let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
+    try clientSocket.write(from: message, to: serverAddress)
+
+
+} catch let error {
+    print("Connection error \(error)")
+    
+}
