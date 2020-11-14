53481424b1fbcacdde0ba50e0abc09503144d1a5
ja.toctaguano <ja.toctaguano@alumnos.urjc.es>
2020-11-14 01:44:32 +0100

    mods

--- Sources/echo-client/main.swift
+++ Sources/echo-client/main.swift
@@ -2,21 +2,35 @@
 
 import Socket
 import Glibc
+import Foundation
 
-let server = "localhost"
-let port: Int32 = 7667
-let message = "Hello, world!"
+// let server = "localhost"
+// let port: Int32 = 7667
+// let message = "Hello, world! Again!"
 
 do {
+
+    let clientInfo = CommandLine.arguments
+    let server = clientInfo[1]
+    let port = Int32(clientInfo[2])!
+
     guard let serverAddress = Socket.createAddress(for: server, on: port) else {
         print("Error creating Address")
         exit(1)
         
     }
-
     let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
-    try clientSocket.write(from: message, to: serverAddress)
+    var quit = false
+    repeat {
+        let message = readLine()!
+        if message == ".quit" {
+            quit = true
 
+        }else{
+            try clientSocket.write(from: message, to: serverAddress)
+        }
+        
+    }while !quit
 
 } catch let error {
     print("Connection error \(error)")
