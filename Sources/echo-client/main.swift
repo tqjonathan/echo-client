7f7efbf5d5e1ef3323b983c9706fc14bfc50c0c6
ja.toctaguano <ja.toctaguano@alumnos.urjc.es>
2020-11-15 20:06:18 +0100

    mods

--- Sources/echo-client/main.swift
+++ Sources/echo-client/main.swift
@@ -4,9 +4,7 @@ import Socket
 import Glibc
 import Foundation
 
-// let server = "localhost"
-// let port: Int32 = 7667
-// let message = "Hello, world! Again!"
+
 
 do {
 
@@ -19,15 +17,23 @@ do {
         exit(1)
         
     }
+    // Crea el socket de cliente, no cambiar parametros
     let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
     var quit = false
+    var buffer = Data(capacity: 1000) //
     repeat {
         let message = readLine()!
         if message == ".quit" {
             quit = true
 
         }else{
+            // escribe en el socket, el mensaje y el destino
             try clientSocket.write(from: message, to: serverAddress)
+            let (_, _) = try clientSocket.readDatagram(into: &buffer) //coge los datos recibidos
+            let message = String(decoding: buffer, as: UTF8.self)
+            print(message)
+            buffer.removeAll()
+        
         }
         
     }while !quit
