13c43185e5868d5422e9805c49e8dccb3ecd21d2
ja.toctaguano <ja.toctaguano@alumnos.urjc.es>
2020-11-17 14:58:00 +0100

    .

--- Sources/echo-client/main.swift
+++ Sources/echo-client/main.swift
@@ -4,14 +4,19 @@ import Socket
 import Glibc
 import Foundation
 
+// let server = "localhost"
+// let port: Int32 = 7667
 
+// struct Point {
+//     var x: Double
+//     var y: Double
+// }
 
 do {
 
     let clientInfo = CommandLine.arguments
     let server = clientInfo[1]
     let port = Int32(clientInfo[2])!
-
     guard let serverAddress = Socket.createAddress(for: server, on: port) else {
         print("Error creating Address")
         exit(1)
@@ -20,7 +25,16 @@ do {
     // Crea el socket de cliente, no cambiar parametros
     let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
     var quit = false
-    var buffer = Data(capacity: 1000) //
+    var buffer = Data(capacity: 1000)
+
+        ///PARTE NUEVA
+    //accede a memoria y el contenido lo vuelca en buffer
+    // "hello".utf8CString.withUnsafeBytes { buffer.append(contentsOf: $0) }
+    // withUnsafeBytes(of: Point(x: 0.7, y: -1.25)) { buffer.append(contentsOf: $0) }
+    // withUnsafeBytes(of: 42) { buffer.append(contentsOf: $0) }
+    // envia el contenido del buffer al server
+    // try clientSocket.write(from: buffer, to: serverAddress)
+
     repeat {
         let message = readLine()!
         if message == ".quit" {
@@ -29,7 +43,7 @@ do {
         }else{
             // escribe en el socket, el mensaje y el destino
             try clientSocket.write(from: message, to: serverAddress)
-            let (_, _) = try clientSocket.readDatagram(into: &buffer) //coge los datos recibidos
+            (_, _) = try clientSocket.readDatagram(into: &buffer) //coge los datos recibidos
             let message = String(decoding: buffer, as: UTF8.self)
             print(message)
             buffer.removeAll()
